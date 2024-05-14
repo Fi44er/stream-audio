603e3c217d6f3f7a50da1333b0990f49ffd5ab85
iO <senpai9464@yandex.ru>
2024-05-14 16:31:45 +0500

    +

--- frontend/src/components/Form/form.tsx
+++ frontend/src/components/Form/form.tsx
@@ -1,9 +1,10 @@
+import blurIcon from '../../assets/img/linesForm.svg';
 import style from './form.module.sass';
 import {IStateForm} from './types/types';
-
 export const Form = ({title, children, onClick}: IStateForm): JSX.Element => (
 	<div className={style.form}>
 		<h2>{title}</h2>
+		<img src={blurIcon} className={style.blur}/>
 		<div className={style.inputs}>
 			{children}
 		</div>
--- frontend/src/pages/Auth/login/login.tsx
+++ frontend/src/pages/Auth/login/login.tsx
@@ -1,3 +1,4 @@
+
 import {useRouter} from '@tanstack/react-router';
 import {useState} from 'react';
 import {Form} from '../../../components/Form/form';
@@ -12,21 +13,42 @@ export const Login = (): JSX.Element => {
 		password: '',
 		passwordRepeat: '',
 	});
-	function login() {
-		setUser(stateLogin);
-		const state = getUser();
-		if (state) {
+	const [code, setCode] = useState<number>();
+	function login(step: number) {
+		if (step === 1 && checkState()) {
+			console.log(code);
+			// Логика ответа сервера
 			router.navigate({to: '/'});
-			console.log(state);
 		}
+
+		setUser(stateLogin);
 	}
+
+	const checkState = (): boolean => {
+		const state = getUser();
+		if (state.email === '' || state.password === '' || state.passwordRepeat === '') {
+			return false;
+		}
+
+		return true;
+	};
 
 	return (
 		<div className={style.pageLogin}>
-			<Form title='Регистрация' onClick={login}>
-				<input key='email' type='text' placeholder='Email' onChange={e => setStateLogin({...stateLogin, email: e.target.value})} />
-				<input key='password' type='text' placeholder='Password' onChange={e => setStateLogin({...stateLogin, password: e.target.value})}/>
-				<input key='passwordRepeat' type='text' placeholder='PasswordRepeat' onChange={e => setStateLogin({...stateLogin, passwordRepeat: e.target.value})}/>
+			<Form title='Регистрация' onClick={login.bind(null, 1) }>
+				{
+					checkState() ? (
+						<>
+							<input key='code' type='text' placeholder='Code' onChange={e => setCode(Number(e.target.value))}/>
+						</>
+					) : (
+						<>
+							<input key='email' type='text' placeholder='Email' onChange={e => setStateLogin({...stateLogin, email: e.target.value})} />
+							<input key='password' type='text' placeholder='Password' onChange={e => setStateLogin({...stateLogin, password: e.target.value})}/>
+							<input key='passwordRepeat' type='text' placeholder='PasswordRepeat' onChange={e => setStateLogin({...stateLogin, passwordRepeat: e.target.value})} />
+						</>
+					)
+				}
 			</Form>
 		</div>
 	);
